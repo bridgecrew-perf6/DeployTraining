7445228b9be667d95bcc9bf4839c7d0cf0dee312
Max Nikiforov <maxxnikiforov@gmail.com>
2022-06-30 21:24:42 +0700

    work with node.js reading node_modules

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+const fs = require('fs');
+
+// eslint-disable-next-line no-console
 console.log({
   answer: 42,
+});
+
+fs.readdir('./node_modules', { withFileType: true }, (error, dirEntryList) => {
+  if (!error) {
+    dirEntryList.forEach((element) => {
+      console.log(element);
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
 });
